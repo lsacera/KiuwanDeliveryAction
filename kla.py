e6347f97adf1100ded2bef899b337210e7e502aa
Eduardo Boronat <eduardo.boronat@kiuwan.com>
2020-06-10 12:24:15 +0200

    SSO Support

--- kla.py
+++ kla.py
@@ -75,7 +75,7 @@ def getBLAnalysisCodeFromKLAOutput(output_to_parse):
     return output_to_parse.split("Analysis created in Kiuwan with code:", 1)[1].split()[0]
 
 # Function to call the Kiuwan API to get the actual URL
-def getBLAnalysisResultsURL(a_c, kla_user=PARAM_KLA_USERNAME, kla_password=PARAM_KLA_PASSWORD):
+def getBLAnalysisResultsURL(a_c, kla_user=PARAM_KLA_USERNAME, kla_password=PARAM_KLA_PASSWORD, advanced=PARAM_KLA_ADVANCEDPARAMS):
     apicall = "https://api.kiuwan.com"
     if not PARAM_KLA_KIUWANBASEURL:
       apicall = PARAM_KLA_KIUWANBASEURL + "/saas/rest/v1"
@@ -83,7 +83,23 @@ def getBLAnalysisResultsURL(a_c, kla_user=PARAM_KLA_USERNAME, kla_password=PARAM
     apicall = apicall + "/apps/analysis/" + a_c
     print('Calling REST API [', apicall, '] ...')
 
-    response = requests.get(apicall, auth=requests.auth.HTTPBasicAuth(kla_user, kla_password))
+    authString = base64.encodebytes(('%s:%s' % (kla_user,kla_password)).encode()).decode().strip()
+    
+    if "domain-id" in advanced:
+      posDomain = advanced.find("domain-id")
+      value_domain_id = advanced[posDomain+10:] ##remove domain-id word
+      posWhitespace = value_domain_id.find(" ")
+      if posWhitespace != -1:
+        value_domain_id = value_domain_id[:posWhitespace]
+      my_headers = {
+          "Authorization": 'Basic {}'.format(authString),
+          "X-KW-CORPORATE-DOMAIN-ID": value_domain_id
+      }
+    else:
+      my_headers = {
+        "Authorization": 'Basic {}'.format(authString)
+      }
+    response = requests.get(url=apicall,headers=my_headers)
 
     print(response)
     print('Contenido', response.content)
